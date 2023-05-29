6fc0eebd62b074365264bc93441b638e6a639c96
Ross Bratton <bratton.ross@gmail.com>
2023-05-29 21:44:14 +0100

    rust: next_token

---
+++ rust/src/lexer/lexer.rs
@@ -0,0 +1,109 @@
+use anyhow::Result;
+
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Illegal,
+    Eof,
+
+    // Identifiers + literals
+    Ident(String),
+    Int(String),
+
+    // Operators
+    Assign,
+    Plus,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    // Keywords
+    Function,
+    Let,
+}
+
+pub struct Lexer {
+    input: Vec<u8>,
+    position: usize,
+    read_position: usize,
+    ch: u8,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Lexer {
+        let mut lexer = Lexer {
+            input: input.into_bytes(),
+            position: 0,
+            read_position: 0,
+            ch: 0,
+        };
+        lexer.read_char();
+        return lexer;
+    }
+
+    pub fn next_token(&mut self) -> Result<Token> {
+        let token = match self.ch {
+            b'=' => Token::Assign,
+            b';' => Token::Semicolon,
+            b'(' => Token::LParen,
+            b')' => Token::RParen,
+            b',' => Token::Comma,
+            b'+' => Token::Plus,
+            b'{' => Token::LBrace,
+            b'}' => Token::RBrace,
+            0 => Token::Eof,
+            _ => todo!(),
+        };
+
+        self.read_char();
+        return Ok(token);
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = 0;
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Lexer, Token};
+    use anyhow::Result;
+
+    #[test]
+    fn test_next_token() -> Result<()> {
+        let input = "=+(){},;";
+
+        let tests = vec![
+            Token::Assign,
+            Token::Plus,
+            Token::LParen,
+            Token::RParen,
+            Token::LBrace,
+            Token::RBrace,
+            Token::Comma,
+            Token::Semicolon,
+            Token::Eof,
+        ];
+
+        let mut lexer = Lexer::new(input.to_string());
+
+        for expected_token in tests {
+            let token = lexer.next_token()?;
+            assert_eq!(token, expected_token);
+        }
+
+        return Ok(());
+    }
+}
