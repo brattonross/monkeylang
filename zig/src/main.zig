bf3eb39094216f55377a1604277b16f8ab34a7a2
Ross Bratton <bratton.ross@gmail.com>
2024-10-07 20:46:55 +0100

    zig: 1.5

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,13 +1,32 @@
 const std = @import("std");
+const Lexer = @import("Lexer.zig");
+const Token = @import("Token.zig");
+
+const prompt = ">> ";
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
 
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const alloc = gpa.allocator();
+    const username = try std.process.getEnvVarOwned(alloc, "USER");
+    defer alloc.free(username);
 
-    try bw.flush();
+    try stdout.print("Hello {s}! This is the Monkey programming language!\n", .{username});
+    try stdout.print("Feel free to type in commands\n", .{});
+
+    while (true) {
+        try stdout.print("{s}", .{prompt});
+        var buf: [1024]u8 = undefined;
+        if (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |input| {
+            var lexer = Lexer.init(input);
+            var token = lexer.nextToken();
+            while (token.type != .eof) : (token = lexer.nextToken()) {
+                try stdout.print("{}\n", .{token});
+            }
+        }
+    }
 }
